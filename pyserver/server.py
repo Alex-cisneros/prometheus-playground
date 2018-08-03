d5e43956424dd4dd3cf1f7ecaf41aa120eb3fd21
alex-cisneros <alejandro.cisneros.escalante@gmail.com>
2018-08-03 18:57:41 -0500

    changes to server.py

--- pyserver/server.py
+++ pyserver/server.py
@@ -1,7 +1,10 @@
 import http.server
 import random
+import time
 from prometheus_client import start_http_server
 from prometheus_client import Counter
+from prometheus_client import Gauge
+from prometheus_client import Summary
 
 
 REQUESTS = Counter('hello_worlds_total',
@@ -13,17 +16,36 @@ SALES = Counter('hello_world_sales_euro_total',
 EXCEPTIONS = Counter('hello_world_exceptions_total',
             'Exceptions serving Hello World.')
 
+INPROGRESS = Gauge('Hello_worlds_inprogress',
+                'Number of Hello Worlds in progress.')
+
+LAST = Gauge('Hello_world_last_time_seconds',
+            'The last time a Hello World was served.')
+
+LATENCY = Summary('hello_world_latency_seconds',
+            'Time for a request Hello World.')
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
+    @INPROGRESS.track_inprogress()
     def do_GET(self):
+        start = time.time()
         REQUESTS.inc()
+        # INPROGRESS.inc()
         with EXCEPTIONS.count_exceptions():
             if random.random() < 0.2:
+                # INPROGRESS.dec()
+                LATENCY.observe(time.time() - start)
                 raise Exception
+                
         euros = random.random()
         SALES.inc(euros)
         self.send_response(200)
         self.end_headers()
         self.wfile.write("Hello World for {} euros.".format(euros).encode())
+        # LAST.set(time.time())
+        LAST.set_to_current_time()
+        # INPROGRESS.dec() 
+        LATENCY.observe(time.time() - start)
 
 if __name__ == "__main__":
     start_http_server(8000)
